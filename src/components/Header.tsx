4e18fc03b4d6e865e3717ec46da22c8b5da81c11
Bryan Chan <103339794+bchan9803@users.noreply.github.com>
2024-02-09 21:07:08 -0800

    ...

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,6 @@ const Header = () => {
 
   const logout = () => {
     setCookies('access_token', '');
-    // window.localStorage.clear("userId");
     window.localStorage.clear();
     navigate('/');    // redirect to home page
     navigate(0);
@@ -24,7 +23,7 @@ const Header = () => {
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
           </div>
           <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 text-lg">
-            <li><Link reloadDocument to='/import-recipes'>Recipes</Link></li>
+            <li><Link reloadDocument to='/recipes'>Recipes</Link></li>
             <li><Link reloadDocument to='/about'>About</Link></li>
 
             {/* display logout btn if user is logged in */}
